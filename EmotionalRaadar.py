8552bbe74e46dd3428d35630f9972c64e1015561
shebss <113125867+shebss@users.noreply.github.com>
2022-10-01 20:21:44 -0400

    Add files via upload

---
+++ EmotionalRaadar.py
@@ -0,0 +1,24 @@
+
+import streamlit as st
+import random
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+st.title('This is a Tweeter Outrage Radar.')
+st.text('Gotta emotionally discard daily Internet bull****')
+    
+tweet = st.text_input('Now please gently input the tweet:')
+
+whenTweetBad = ["this is unsafe", "not recommended", "risky, buddy", "eyebleach please", "this is toxic","NSFW", "emotional Damage", ":(","why do we exist, just to suffer"]
+whenTweetGoodOrNeutral = ["wholesome", "approved", "certified", "10/10", "safe"]
+
+
+
+if tweet == "lil shithead, ur banned":
+    st.text("bad,")
+    st.text(random.choice(whenTweetBad))
+
+pad = st.text('lol')
