7527a5f645c1afabd7188179ab376291ed1e96d3
lucymao-12 <lucymao12@gmail.com>
2022-10-01 20:08:48 -0400

    input prediction

--- LandingPage.py
+++ LandingPage.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from flask import Flask
 import pandas as pd
 import numpy as np
@@ -9,6 +10,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from flask import request, render_template
+from sklearn.naive_bayes import MultinomialNB
 
 
 #nltk.download()
@@ -19,11 +22,13 @@ def lists():
     data_train=pd.read_csv("Corona_NLP_train.csv" ,encoding ='latin1')
 
 
-    print(data_train.columns)
+    #print(data_train.columns)
     #data_train
 
     features_og = data_train.iloc[:, 4].values
     labels = data_train.iloc[:, 5].values
+    
+    #print(labels[31000:])
 
 
     features_train = []
@@ -37,8 +42,50 @@ def lists():
         feature_train=feature_train.lower()
         features_train.append(feature_train)
 
+    vectorizer = TfidfVectorizer(max_features=2500, min_df=7, max_df=0.8, stop_words=stopwords.words('english'))
+    
+    #print(len(features_train))
+    features_train = features_train[31000:]
+    labels = labels[31000:]
 
+    #print(type(features_train))
+    
+    
 
+    features = vectorizer.fit_transform(features_train).toarray()
+    #new_feature=vectorizer.transform(new_value).toarray()
+
+    #for i in features:
+        #print(i)
+
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=0)
+
+    #test = X_test[0]
+
+    text_classifier = RandomForestClassifier(n_estimators=200, random_state=0)
+    text_classifier.fit(X_train, y_train)
+    #text_classifier.
+
+    #print(type(text_classifier))
+
+    pred = text_classifier.predict(X_test)
+
+    #print(pred[-1])
+
+    new_value="this is an informative tweet nothing to see here just some words no emotions"
+    features_train.append(new_value)
+    np.append(labels, "Negative")
+    features = vectorizer.fit_transform(features_train).toarray()
+    print(text_classifier.predict([features[-1]]))
+    #print(confusion_matrix(y_test, pred))
+    #print(classification_report(y_test, pred))
+    print(accuracy_score(y_test, pred))
+
+    return text_classifier
+
+lists()
+
+'''
     for i in range(len(labels)):
         if labels[i] == "Neutral":
             labels[i] = 0
@@ -53,36 +100,25 @@ def lists():
 
     for i in labels:
         print(i)
+'''
+    #return (features_train, labels)
 
-    return (features_train, labels)
-    '''
-    vectorizer = TfidfVectorizer(max_features=2500, min_df=7, max_df=0.8, stop_words=stopwords.words('english'))
-    features = vectorizer.fit_transform(features_train).toarray()
-
-
-
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=0)
-
-    text_classifier = RandomForestClassifier(n_estimators=200, random_state=0)
-    text_classifier.fit(X_train, y_train)
-
-    pred = text_classifier.predict(X_test)
-
-    print(confusion_matrix(y_test, pred))
-    print(classification_report(y_test, pred))
-    print(accuracy_score(y_test, pred))
+    
     #print(pred)
 
-
-
-
-
+'''
     #for i in features:
         #print(i)
 
-    app = Flask(__name__)
 
-    @app.route("/")
-    def hello_world():
-        return "<p>Hello, world</p>"
-    '''
+
+app = Flask(__name__)
+
+@app.route("/")
+def in_text():
+    return render_template("site.html")
+
+@app.route('/', )
+def site():
+'''
+    
